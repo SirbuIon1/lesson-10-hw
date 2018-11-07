91bd761508241791ce34e1b629e390c7c9fc236e
Reiel <niphilim11@gmail.com>
2018-11-07 19:53:54 +0300

    lesson 10 homework loops

--- loop.js
+++ loop.js
@@ -4,7 +4,7 @@ for (var i = 3; i <= 26; i += 3) {
     var a = i;
 }
 if (a % 3 === 0) {
-    alert(a);
+    alert("for loop age is: " + a);
 }
 console.log('while');
 var x = 3;
@@ -13,7 +13,7 @@ while (x <= 26) {
     var b = x;
     x += 3;
 }
-alert(b);
+alert("while loop age is  " + b);
 console.log('do while');
 var n = 3;
 do {
@@ -21,7 +21,7 @@ do {
     n += 3;
     var c = n;
 } while (n < 26);
-alert(c);
+alert("do while loop " + c);
 console.log('for in');
 var m = ['0','21','22','3','14','15','16','7','8',9,10,11,2,13,4,5,6,17,18,19,20,1,12,23,24,25];
 var d ;
@@ -30,4 +30,4 @@ for (d in m) {
     var y = m[d];
     console.log('d', d,'index = ', m[d]);
   }
-} alert(y);
+} alert("for in loop " + y);
