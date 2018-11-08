14a416edf7957eff5dadf0028b1e40dcaa9c0dcb
Reiel <niphilim11@gmail.com>
2018-11-08 12:10:50 +0300

    changed js file new var myCurentAge

--- loop.js
+++ loop.js
@@ -1,5 +1,7 @@
+var myCurentAge = 26;
+
 console.log('for loop');
-for (var i = 3; i <= 26; i += 3) {
+for (var i = 3; i <= myCurentAge; i += 3) {
     console.log('i = ', i);
     var a = i;
 }
@@ -8,7 +10,7 @@ if (a % 3 === 0) {
 }
 console.log('while');
 var x = 3;
-while (x <= 26) {
+while (x < myCurentAge) {
     console.log('x = ', x);
     var b = x;
     x += 3;
@@ -19,15 +21,16 @@ var n = 3;
 do {
     console.log('n=', n);
     n += 3;
+    if (n > 24) { break; }
     var c = n;
-} while (n < 26);
+} while (n < myCurentAge);
 alert("do while loop " + c);
 console.log('for in');
-var m = ['0','21','22','3','14','15','16','7','8',9,10,11,2,13,4,5,6,17,18,19,20,1,12,23,24,25];
-var d ;
+var m = ['0', '21', '22', '3', '14', '15', '16', '7', '8', 9, 10, 11, 2, 13, 4, 5, 6, 17, 18, 19, 20, 1, 12, 23, 24, 25];
+var d;
 for (d in m) {
     if (m[d] % 3 === 0) {
-    var y = m[d];
-    console.log('d', d,'index = ', m[d]);
-  }
+        var y = m[d];
+        console.log('d', d, 'index = ', m[d]);
+    }
 } alert("for in loop " + y);
